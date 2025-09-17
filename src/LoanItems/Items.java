5d73d0aabe09704948cfce971b7e73efe9939003
Frederick <frdu0001@stud@ek.dk>
2025-09-17 09:38:21 +0200

    Update 02 "Items class"

--- src/LoanItems/Items.java
+++ src/LoanItems/Items.java
@@ -4,15 +4,31 @@ class Items {
     protected String type;
     protected String title;
 
-    public Items(String Type, String Title){
-        this.type=Type;
-        this.title=Title;
+    public Items(String Type, String Title) {
+        this.type = Type;
+        this.title = Title;
     }
+
     //getters
-    public String getType() { return type; }
-    public String getTitle() { return title; }
+    public String getType() {
+        return type;
+    }
+
+    public String getTitle() {
+        return title;
+    }
 
     //setters
-    public String setType() { return type; }
-    public String setTitle() { return title; }
+    public String setType() {
+        return type;
+    }
+
+    public String setTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + title + "\nType: " + type + "\n";
+    }
 }
