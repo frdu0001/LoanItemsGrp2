d40daa2b49f7f76337e7e0cc6d6687d435ec30ea
Frederick <frdu0001@stud@ek.dk>
2025-09-17 09:36:17 +0200

    Update 01 "Items class"

--- src/LoanItems/Items.java
+++ src/LoanItems/Items.java
@@ -1,4 +1,18 @@
 package LoanItems;
 
-public class Items {
+class Items {
+    protected String type;
+    protected String title;
+
+    public Items(String Type, String Title){
+        this.type=Type;
+        this.title=Title;
+    }
+    //getters
+    public String getType() { return type; }
+    public String getTitle() { return title; }
+
+    //setters
+    public String setType() { return type; }
+    public String setTitle() { return title; }
 }
