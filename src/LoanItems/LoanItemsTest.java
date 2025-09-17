e0b1c1a245c7ece95d20e02f4ea829446ace8eff
Marcus Bramsen <k4wkv49@gmail.com>
2025-09-17 11:35:42 +0200

    Test update

--- src/LoanItems/LoanItemsTest.java
+++ src/LoanItems/LoanItemsTest.java
@@ -6,7 +6,7 @@ public class LoanItemsTest {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Velkomstbesked og spørger hvor mange genstande der skal lånes
+        // Welcome message and asks how many items which needs to be rented
         System.out.println("Welcome to the Professors Library!" + "\n-----------");
         System.out.println("How many items would you like to rent?");
         int numberOfItems = scanner.nextInt();
@@ -18,43 +18,42 @@ public class LoanItemsTest {
             numberOfItems = scanner.nextInt();
         }
 
-        // Opretter et array til at holder lånte genstande
+        // Creates the array which contains the rented items
         Items[] item = new Items[numberOfItems];
 
-        // Spørger om data på hver genstand og gemmer dem i arrayet
+        // Asks user for data on each item and saves to the array
         for (int i = 0; i < numberOfItems; i++) {
-            System.out.print("What type is the items of your choise?\n");
+            System.out.print("What type is the item of your choise?\n");
             String type = scanner.nextLine();
 
-            // || !type.equals("Book") || !type.equals("video") || !type.equals("Video")
-
-            while (!type.equalsIgnoreCase("book")) {
-                System.out.println("Unknown entity! This library only has books or videos.");
+            // Quick fix to check if the type is NOT book or video
+            if (type.equalsIgnoreCase("book")) {
+            } else if (type.equalsIgnoreCase("video")) {
+            } else {
+                System.out.println("Unknown entity! This library only handles books and videos.");
                 type = scanner.nextLine();
             }
 
             System.out.print("What is the name of the book or video?\n");
             String title = scanner.nextLine();
 
-            // Opretter den rigtige genstand baseret på typen
+            // Creates the right items based on the type
             if (type.equalsIgnoreCase("Book")) {
                 item[i] = new Book(title, type);
             } else if (type.equalsIgnoreCase("Video")) {
                 item[i] = new Video(title, type);
             } else {
-                System.out.println("Unknown entity! This library only has books or videos.");
-
-                //item[i] = new Items(title, type);
+                System.out.println("Unknown entity! This library only handles books and videos.");
             }
         }
 
-        System.out.println("Tak for at låne hos os! Her er en oversigt over de bøger/videoer du har lånt :\n");
+        System.out.println("Thank you for renting with us. Here is a list of your rented items: \n");
         scanner.close();
 
-        // Printer alle genstande som er lånt - Her tæller den en op på ID'et for hver genstand.
-        System.out.println("LIST OF LOANED ITEMS: \n" + "-----------------");
+        // Prints all the items for rent - It counts up the ID for each item.
+        System.out.println("LIST OF RENTED ITEMS: \n" + "-----------------\n" + "ID\tTYPE\tTITLE");
         for (int i = 0; i < numberOfItems; i++) {
-            System.out.println("Registreret genstand nr: " + (i + 1) + "\n" + item[i] + "\n");
+            System.out.println((i + 1) + " " + item[i] + "\n");
         }
     }
 }
