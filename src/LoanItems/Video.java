26b94d05d13dab8af450cef1f1c7e1ccf242e08e
johanoliverlarsen <johanoliverlarsen@gmail.com>
2025-09-17 09:32:04 +0200

    Getters + Setters

--- src/LoanItems/Video.java
+++ src/LoanItems/Video.java
@@ -7,6 +7,27 @@ class video extends items {
         super(title, type);
     }
 
+    //Getters
+
+    public String getTitle (){
+        return title;
+    }
+
+    public String getType (){
+        return type;
+    }
+
+    //Setters
+
+    public String setType (){
+        return type;
+    }
+
+    public String setType (){
+        return type;
+    }
+
+
     @Override
     public String toString() {
         return "Title: " + title + "\nType: " + type + "\n";
